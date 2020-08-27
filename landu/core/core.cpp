cbc169c22b7e16fc2f1a6dcae1a33fd529ddd45c
Michael Shishov <63235817+FANDIM-wolf@users.noreply.github.com>
2020-08-27 16:27:23 +0300

    Add files via upload

--- landu/core/core.cpp
+++ landu/core/core.cpp
@@ -7,6 +7,36 @@
 
 using namespace std;
 
+//create txt file for user data
+
+void create_txt_file_user_data() {
+
+string name_user_data , password_user_data , answer1;
+
+//variables to save user data
+string user_name , user_password;
+
+cout<<"create file:";
+cin>>answer1;
+if(answer1 == "yes"){
+  ofstream outfile("data.txt");
+  cout<<"Enter your name:";
+  cin>>name_user_data;
+  cout<<"Enter your password:";
+  cin>>password_user_data;
+  outfile<<name_user_data;
+  outfile<<password_user_data;
+}
+else {
+	cout<<"GO"<<endl;
+}
+
+user_name = name_user_data;
+user_password = password_user_data;
+
+}
+
+
 //commandline console to mine currency
 void command_line_console(){
 //variables for command line 
@@ -23,10 +53,14 @@ if(command=="--version"){
     command_line_console();
 
 }
-if(command==""){
+if(command=="--version_landu"){
 	//open file and show version
     cout<<"version:0.0.1"<<endl;
     command_line_console();
+}
+if(command=="log_in"){
+	create_txt_file_user_data(); // create user data;
+	command_line_console();
 }
 
 
